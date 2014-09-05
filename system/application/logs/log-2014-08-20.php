a0294f53033fd06eb49f95c046b611691d327b50
Jepri Sinaga <therexxar@gmail.com>
2014-09-05 21:32:43 +0700

    init apps

---
+++ system/application/logs/log-2014-08-20.php
@@ -0,0 +1,26 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-08-20 14:35:31 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:37:42 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:38:15 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:41:19 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:44:26 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:44:47 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:45:05 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:45:27 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:48:17 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:49:15 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:50:14 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:51:54 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:52:16 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:54:48 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:55:17 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:55:19 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:55:48 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:56:57 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 14:58:10 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 15:00:45 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 15:01:15 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 15:03:03 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 15:03:31 --> 404 Page Not Found --> 
+ERROR - 2014-08-20 15:27:07 --> 404 Page Not Found --> 
